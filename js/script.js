14d3db859823d25d085ab99f7b1f066f7f7b0a62
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-11 23:43:31 +0200

    funzionamento-corretto-gioco-dei-dadi

--- js/script.js
+++ js/script.js
@@ -7,32 +7,38 @@
 //GIOCO DEI DADI
 
 //generiamo randomicamente un numero da 1 a 6 per il giocatore e il computer
+const dicePlay = document.getElementById('dice-game');
 
-let playerDice1 = Math.round (Math.random () *6);
-let cpuDice1 = Math.round (Math.random () *6);
+dicePlay.addEventListener('click', function(){ 
 
+let playerDice1 = Math.floor (Math.random () *6) + 1;
+let cpuDice1 = Math.floor (Math.random () *6) + 1;
+const firstDiceImage = 'img/dice' + playerDice1 + '.png'
+const secondDiceImage = 'img/dice' + cpuDice1 + '.png'
 
 console.log(playerDice1, cpuDice1);
 
-//diamo un'immagine al dado che uscirà
-const firstDiceImage = 'img/dice' + playerDice1 + '.png'
-const secondDiceImage = 'img/dice' + cpuDice1 + '.png'
+
+ //diamo un'immagine al dado che uscirà
 
 document.querySelectorAll('img')[0].setAttribute ('src', firstDiceImage); 
 document.querySelectorAll('img')[1].setAttribute ('src', secondDiceImage); 
 
-if (playerDice1 > cpuDice1) {
-  document.querySelector ('.winner').innerHTML = 'The Winner is Player1. Congratulation!';
-}
-
-else if (playerDice1 < cpuDice1) {
-  document.querySelector ('.winner').innerHTML = 'The Winner is King Dice. Pathetic!';
-}
-
-else if (playerDice1 = cpuDice1) {
-  document.querySelector ('.winner').innerHTML = 'Draw? Is Possible?';
-}
-
+  // se il mio dado è superiore al cpu, vinci
+  if (playerDice1 > cpuDice1) {
+    document.querySelector ('.winner').innerHTML = 'The Winner is Player1. Congratulation!';
+  }
+  
+  //se il mio dado è inferiore al cpu, perdi
+  else if (playerDice1 < cpuDice1) {
+    document.querySelector ('.winner').innerHTML = 'The Winner is King Dice. Pathetic!';
+  }
+  
+  else {
+    document.querySelector ('.winner').innerHTML = 'Draw? Is Possible?';
+  }
+  
+});
 
 
 
